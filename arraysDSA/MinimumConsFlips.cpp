0321994cd81d664477d63f57a32107e96f7eab1e
Shadman <shadman01010@gmail.com>
2022-01-25 21:08:27 +0530

    *Intersting Question*

---
+++ arraysDSA/MinimumConsFlips.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+//#include<bits/stdc++.h>
+using namespace std;
+
+
+void CoinFlips(int arr[] , int n){
+    for(int i=1;i<n;i++){
+
+        if(arr[i]!= arr[i-1]){
+            if(arr[i]!= arr[0]){
+                cout << "From" << i << "to";
+            } else
+            cout << (i-1) << endl;
+        }
+    }
+    if(arr[n-1]!= arr[0])
+    cout << (n-1) << endl;
+
+}
+
+int main()
+{
+    int arr[] = {0,0,1,1,0,0,1,1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    CoinFlips(arr, n);
+
+    return 0;
+}
