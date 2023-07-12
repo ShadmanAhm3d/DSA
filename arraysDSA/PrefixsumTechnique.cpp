9025c2784b7ac05fb5d8f09630db66d0bf962e48
ShadmanAhm3d <shadman01010@gmail.com>
2023-07-12 19:48:55 +0530

    july23

--- arraysDSA/PrefixsumTechnique.cpp
+++ arraysDSA/PrefixsumTechnique.cpp
@@ -1,23 +1,40 @@
-#include <bits/stdc++.h>
-
+#include <cstdint>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int funcc(int arr[] , int n ){
-   
+int PS(int arr[], int n) {
 
+  for (int i = 1; i < n; i++) {
+    arr[i] += arr[i - 1];
+  }
+  return -1;
+}
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " | ";
+  cout << endl;
 }
 
+int main() {
+  int arr[] = {2, 8, 3, 9, 6, 5, 4};
+  int k = 4;
+  int n = sizeof(arr) / sizeof(arr[0]);
+  cout << "Number of Elements Processed in the array: " << n << endl;
 
+  // vector<int> flippedIndexes = solve(arr, n);
+  // cout << "Flipped Indexes: ";
+  // for (int i = 0; i < flippedIndexes.size(); i++) {
+  //   cout << flippedIndexes[i] << " ";
+  // }
+  // cout << endl;
 
+  cout << "Orignal Array: ";
+  printArray(arr, n);
+  PS(arr, n);
+  cout << "Updated Array: ";
+  printArray(arr, n);
 
-
-
-
-
-int main(){
-
-		int arr[] = {1,2,3,44};
-	//int n = sizeof(arr)/sizeof(arr[0]); 
-	
-	return 0;
+  return 0;
 }
+
--- arraysDSA/SlidingWindowTech.cpp
+++ arraysDSA/SlidingWindowTech.cpp
@@ -1,39 +1,68 @@
-//#include <iostream>
-#include<bits/stdc++.h>
+#include <cstdint>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+/* int MaxSum(int arr[], int n , int k){
+      int maxTotal = INT8_MIN;
+      int lenght = n-k+1;
+      for(int i=0 ; i< lenght; i++){
+        int currTotal = 0;
+        for(int j=i ; j <i+ k ; j++){
+          currTotal += arr[j];
 
+       }
+        maxTotal  = max(currTotal,maxTotal);
+      }
+      return maxTotal;
+} */
 
-//A program which takes a array of itegers and a value k and prints out the total maximum value of consective ks
+// currentSum
+// first iterated index
+// last iterated index
 
+int MaxSum(int arr[], int n, int k) {
+  if (n < k) {
+    return n;
+  }
+  int curr_sum = 0;
+  for (int i = 0; i < k; i++) {
+    curr_sum += arr[i];
+  }
+  int maxSum = INT8_MIN;
 
+  for (int i = 1; i < n - k + 1; i++) {
+    // curr_sum = curr_sum - arr[i - 1] + arr[i + k - 1];
+    curr_sum = curr_sum - arr[i - 1] + arr[i + k - 1];
+    maxSum = max(maxSum, curr_sum);
+  }
 
-
- int maxfunc(int arr[], int n , int k){
-     int maxvalue = INT_MIN;
-     for(int i=0; i < n-k +1 ; i++){
-         int currmax  = 0;
-         for(int j =i;j<i+k ;j++){
-             currmax = currmax + arr[j];
-
-         }
-             maxvalue = max(maxvalue, currmax);
-
-     }
-     return maxvalue;
- }
-
-
-
-
-
-
-int main()
-
-{
-
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << maxfunc(arr, n , 2);
-    return 0;
+  return maxSum;
 }
+
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+}
+
+int main() {
+  int arr[] = {1, 4, 2, 10, 2, 3, 1, 0, 20};
+  int k = 4;
+  int n = sizeof(arr) / sizeof(arr[0]);
+  cout << "Number of Elements Processed in the array: " << n << endl;
+
+  // vector<int> flippedIndexes = solve(arr, n);
+  // cout << "Flipped Indexes: ";
+  // for (int i = 0; i < flippedIndexes.size(); i++) {
+  //   cout << flippedIndexes[i] << " ";
+  // }
+  // cout << endl;
+
+  cout << MaxSum(arr, n, k) << endl;
+  cout << "Updated Array: ";
+  printArray(arr, n);
+
+  return 0;
+}
+
