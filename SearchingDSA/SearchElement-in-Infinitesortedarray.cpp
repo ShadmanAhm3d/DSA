89e99e91a78ed16eb1f8316211c73f1a8bf9ca64
ShadmanAhm3d <shadman01010@gmail.com>
2023-07-15 11:11:33 +0530

    july23

--- SearchingDSA/SearchElement-in-Infinitesortedarray.cpp
+++ SearchingDSA/SearchElement-in-Infinitesortedarray.cpp
@@ -17,43 +17,41 @@ using namespace std;
 // 	}
 // 	return -1;
 // }
-int BS(int arr[], int x, int low, int high, int n)
-{
-	while(high > low){
-		int mid = (high+low)/2;
+int BS(int arr[], int x, int low, int high, int n) {
+  while (high > low) {
+    int mid = (high + low) / 2;
 
-		if(arr[mid] == x)
-		return mid;
-		
-		if(arr[mid] > x)
-		high = mid-1;
+    if (arr[mid] == x)
+      return mid;
 
-		if(arr[mid]  < x)
-		low  = mid +1;
-	}
-	return -1;
+    if (arr[mid] > x)
+      high = mid - 1;
+
+    if (arr[mid] < x)
+      low = mid + 1;
+  }
+  return -1;
 }
 
-int Search(int arr[], int n, int x)
-{
-	int high = arr[1];
-	int low = arr[0];
+int Search(int arr[], int n, int x) {
+  int high = arr[1];
+  int low = arr[0];
 
-	while (x >= high)
-	{
-		low = high;
-		high = high * 2;
-	}
-	return BS(arr, x, low, high, n);
+  while (x >= high) {
+    low = high;
+    high = high * 2;
+  }
+  return BS(arr, x, low, high, n);
 }
 
-int main()
-{
+int main() {
 
-	int arr[] = {1, 2, 3, 3, 4, 4, 5, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 9, 12, 12, 12, 12, 12};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = ;
-	int res = Search(arr, n, x);
-	(res == -1) ? cout << "elem not present" : cout << "elem present at index : "<<res;
-	return 0;
+  int arr[] = {1, 2, 3, 3, 4, 4, 5,  6,  6,  7,  7, 8,
+               8, 8, 9, 9, 9, 9, 12, 12, 12, 12, 12};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int x = 5;
+  int res = Search(arr, n, x);
+  (res == -1) ? cout << "elem not present"
+              : cout << "elem present at index : " << res;
+  return 0;
 }
---
+++ SearchingDSA/lowerandupperbound.cpp
@@ -0,0 +1,83 @@
+
+// Competitive Programming Setup
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+//lower bound
+/* int bs(int arr[], int n, int target) {
+
+  int l = 0;
+  int h = n - 1;
+  int ans = n - 1;
+
+  while (l < h) {
+    int m = (h + l) / 2;
+    if (arr[m] >= target) {
+      h = m - 1;
+      ans = m;
+    } else if (arr[m] < target) {
+      l = m + 1;
+    }
+  }
+
+  return ans;
+} */
+
+
+
+
+
+
+//upper bound
+int bs(int arr[], int n, int target) {
+
+  int l = 0;
+  int h = n - 1;
+  int ans = n - 1;
+
+  while (l <= h) {
+    int m = (h + l) / 2;
+    if (arr[m] > target) {
+      h = m -1;
+      ans = m;
+    } else if (arr[m] <= target) {
+      l = m + 1;
+  }
+  }
+
+  return ans;
+}
+
+
+
+
+
+
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " | ";
+  cout << endl;
+}
+
+int main() {
+
+  int arr[] = {1, 2, 3, 3, 7, 8,8,9 , 9, 9,11};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int x = 8;
+  cout << "Original Array: ";
+  printArray(arr, n);
+  cout << endl << "The size of array " << n << endl;
+  cout << bs(arr, n, x) << endl;
+
+  cout << "Updated Array: ";
+  printArray(arr, n);
+
+  return 0;
+}
