780c48baaa7e483554d45acff26a637c46e3c04a
ShadmanAhm3d <shadman01010@gmail.com>
2025-03-24 12:04:08 +0530

    (^_^)

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -1,65 +1,85 @@
-
-#include <bits/stdc++.h>
-#include <pthread.h>
+#include <iostream>
 using namespace std;
 
-void mergesortt(int arr1[], int arr2[], int n, int n2) {
-  //   int c[n+n2];
-  //   int i;
-  //   for(i=0;i<n;i++){
-  //     c[i]= arr1[i];
-  //   }
-  //
-  //   for(i=0;i<n2;i++){
-  //     c[n+i]= arr2[i];
-  //   }
-  //   sort(c,c+n+n2);
-  //   for(i=0;i<(n+n2);i++){
-  //     cout << c[i] << "  " ;
-  //   }
-  // }
+// Merge function: Merges two sorted halves of arr[]
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;  // Size of left half
+    int n2 = r - m;      // Size of right half
 
-  // efficient sools
+    int L[n1], R[n2];    // Temporary arrays
 
-  int j =0;
-  int i=0;
-  while (i < n && j < n2) {
-    if (arr1[i] <= arr2[j]) {
-      cout << arr1[i] << " ";
-      i++;
-    } else {
-      cout << arr2[j] << " ";
-      j++;
+    // Copying data into Left Subarray
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
     }
-  }
-  while (i < n) {
-    cout << arr1[i] << " ";
-    i++;
-  }
 
-  while (j < n2) {
-    cout << arr2[j] << " ";
-    j++;
-  }
+    // Copying data into Right Subarray
+    for (int i = 0; i < n2; i++) {
+        R[i] = arr[m + 1 + i];
+    }
+
+    int i = 0, j = 0, k = l;  // Initial indexes
+
+    // Merging L[] and R[] into arr[]
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy any remaining elements of L[]
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy any remaining elements of R[]
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-// // Function to print an array
-// void printArray(int arr[], int size) {
-//   int i;
-//   for (i = 0; i < size; i++)
-//     cout << arr[i] << " ";
-//   cout << endl;
-// }
-//
-// Driver program to test above functions
+// Merge Sort function
+void mergeSort(int arr[], int l, int r) {
+    if (l >= r) return;  // Base case
+
+    int m = l + (r - l) / 2;  // Middle index
+
+    mergeSort(arr, l, m);      // Sort first half
+    mergeSort(arr, m + 1, r);  // Sort second half
+
+    merge(arr, l, m, r);  // Merge the sorted halves
+}
+
+// Function to print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Driver Function
 int main() {
-  int arr1[] = {64, 25, 12, 22, 11};
-  int arr2[] = {4, 5, 123, 232, 11};
+    int arr[] = {12, 11, 13, 5, 6, 7};  // Example array
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-  int n = sizeof(arr1) / sizeof(arr1[0]);
-  int n2 = sizeof(arr2) / sizeof(arr2[0]);
-  mergesortt(arr1, arr2, n, n2);
-  // cout << "Sorted array: \n";
-  // printArray(arr, n);
-  return 0;
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    mergeSort(arr, 0, size - 1);  // Sorting the array
+
+    cout << "Sorted array: ";
+    printArray(arr, size);
+
+    return 0;
 }
+
